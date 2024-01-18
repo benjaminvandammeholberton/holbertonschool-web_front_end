21c2cdea11d380cf6edede5e9dedc9f6a4737c87
Benjamin Vandamme <6206@holbertonstudents.com>
2024-01-18 18:44:37 +0100

    add: tasks 5 to 14

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,18 @@
+let user = {
+  hobby: 'Calligraphy',
+  favoriteSport: 'Hockey',
+  astrologicalSign: 'Aries',
+  firstName: 'Guillaume',
+  lastName: 'Ialva',
+  location: 'Telaviv',
+  occupation: 'Engineer',
+};
+
+function logWelcomeUser(welcomeString) {
+  return console.log(
+    `${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`
+  );
+}
+
+let bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser('Hello');
